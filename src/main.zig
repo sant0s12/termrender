10b520090a01296f15fb40099ee238f20501a9cc
sant0s12 <diego.delossantos@mailbox.org>
2025-04-29 13:48:21 +0200

    Uncook the term

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,11 @@ const std = @import("std");
 const lib = @import("lib.zig");
 const assert = @import("std").debug.assert;
 
+var cooked_termios: std.os.linux.termios = undefined;
+
+const stdout = std.io.getStdOut().writer().any();
+const stdin = std.io.getStdIn().reader();
+
 const TermBuffer = struct {
     allocator: *const std.mem.Allocator,
     writer: *const std.io.AnyWriter,
@@ -90,28 +95,94 @@ const BouncyX = struct {
     }
 };
 
+fn uncookTerm() void {
+    // Save term attrs to restore later
+    if (std.os.linux.tcgetattr(0, &cooked_termios) < 0) {
+        std.io.getStdErr().writer().print("Could not read term settings :(\n");
+        std.os.exit(1);
+    }
+
+    var uncooked_termios: std.os.linux.termios = cooked_termios;
+
+    // https://www.reddit.com/r/Zig/comments/b0dyfe/polling_for_key_presses/
+    // Input
+    uncooked_termios.iflag.BRKINT = false;
+    uncooked_termios.iflag.ICRNL = false;
+    uncooked_termios.iflag.INPCK = false;
+    uncooked_termios.iflag.ISTRIP = false;
+    uncooked_termios.iflag.IXON = false;
+
+    // Output
+    uncooked_termios.lflag.ECHO = false;
+    uncooked_termios.lflag.ICANON = false;
+    uncooked_termios.lflag.ISIG = false;
+
+    // Non-blocking reads
+    uncooked_termios.cc[@intFromEnum(std.os.linux.V.MIN)] = 0;
+    uncooked_termios.cc[@intFromEnum(std.os.linux.V.TIME)] = 0;
+
+    if (std.os.linux.tcsetattr(0, std.os.linux.TCSA.NOW, &uncooked_termios) < 0) {
+        std.io.getStdErr().writer().print("Could not uncook term :(\n");
+        std.os.exit(1);
+    }
+}
+
+fn cookTerm() void {
+    if (std.os.linux.tcsetattr(0, std.os.linux.TCSA.NOW, &cooked_termios) < 0) {
+        std.io.getStdErr().writer().print("Could not cook term :(\n");
+        std.os.exit(1);
+    }
+}
+
+fn hideCursor(writer: *const std.io.AnyWriter) !void {
+    _ = try writer.write("\x1B[?25l");
+}
+
+fn showCursor(writer: *const std.io.AnyWriter) !void {
+    _ = try writer.write("\x1B[?25h");
+}
+
 pub fn main() !void {
-    const writer = std.io.getStdOut().writer().any();
+    uncookTerm();
+    defer cookTerm();
+
+    try hideCursor(&stdout);
+    defer showCursor(&stdout) catch unreachable;
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
     const allocator = arena.allocator();
-    var buffer: TermBuffer = try TermBuffer.init(&allocator, &writer);
+    var buffer: TermBuffer = try TermBuffer.init(&allocator, &stdout);
 
-    var bouncyX = BouncyX{ .vx = 1, .vy = 1, .termBuffer = &buffer };
+    var bouncy_x = BouncyX{ .vx = 1, .vy = 1, .termBuffer = &buffer };
 
+    var read_buff: [1]u8 = undefined;
     while (true) {
-        _ = try buffer.updateSize();
-        _ = try buffer.clearTerm();
+        // Read input
+        if (try stdin.read(&read_buff) > 0) {
+            switch (read_buff[0]) {
+                'x' => bouncy_x.vx += 1,
+                'y' => bouncy_x.vy += 1,
+                'q' => break,
+                else => {},
+            }
+
+            read_buff[0] = 0x0;
+        }
+
+        try buffer.updateSize();
+        try buffer.clearTerm();
         // _ = buffer.clearBuffer();
 
         // Draw stuff
-        bouncyX.tick();
-        try bouncyX.draw();
+        bouncy_x.tick();
+        try bouncy_x.draw();
 
         // _ = try buffer.draw();
 
         std.time.sleep(10 * std.time.ns_per_ms);
     }
+
+    try buffer.clearTerm();
 }
